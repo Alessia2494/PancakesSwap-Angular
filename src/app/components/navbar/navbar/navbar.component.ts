74d1f1979ab2e6c9ca16eb002491be0e8c7c9493
Alessia Russo <r.alessia2494@gmail.com>
2022-04-06 11:59:50 +0200

    add navmodelpart1

--- src/app/components/navbar/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { language } from 'src/app/models/modelNavbar';
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +7,93 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+
+  languages:language[]=[
+    {
+      linguage:"العربي"
+    },
+    {
+      linguage: "বাংলা"
+    },
+    {
+      linguage: "English"
+    },
+    {
+      linguage: "Deutsch"
+    },
+    {
+      linguage: "Ελληνικά"
+    },
+    {
+      linguage:"Español"
+    },
+    {
+      linguage:"Suomalainen"
+    },
+    {
+      linguage:"Filipino"
+    },
+    {
+      linguage:"Français"
+    },
+    {
+      linguage:"हिंदी"
+    },
+    {
+      linguage:"Magyar"
+    },
+    {
+      linguage:"Bahasa Indonesia"
+    },
+    {
+      linguage:"Italiano"
+    },
+    {
+      linguage:"日本語"
+    },
+    {
+      linguage:"한국어"
+    },
+    {
+      linguage:"Nederlands"
+    },
+    {
+      linguage:"Polski"
+    },
+    {
+      linguage:"Português(Brazil)"
+    },
+    {
+      linguage:"Português"
+    },
+    {
+      linguage:"Română"
+    },
+    {
+      linguage:"Русский"
+    },
+    {
+      linguage:"Svenska"
+    },
+    {
+      linguage:"தமிழ்"
+    },
+    {
+      linguage:"Türkçe"
+    },
+    {
+      linguage:"Українська"
+    },
+   {
+    linguage:"Tiếng Việt"
+   },
+   {
+    linguage:"简体中文"
+   },
+   {
+    linguage:"繁體中文"
+   }
+ ]
 
   constructor() { }
 
