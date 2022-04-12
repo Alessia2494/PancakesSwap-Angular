b046b85d34fa3dfee28f16985356857d65b32e86
Matteo Musti <94116882+MatteoMusti@users.noreply.github.com>
2022-04-12 19:36:12 +0200

    fixed: statistics

--- src/app/components/section-6/sec6-statistics/sec6-statistics.component.ts
+++ src/app/components/section-6/sec6-statistics/sec6-statistics.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Statistic } from 'src/app/models/statistics';
+import { Statistic } from 'src/app/models/Statistics';
 
 @Component({
   selector: 'app-sec6-statistics',
