f3b119bbe6c5ef5f0f706444a231083d807caf32
Matteo Musti <94116882+MatteoMusti@users.noreply.github.com>
2022-04-12 17:13:44 +0200

    add: section-6 models

--- src/app/components/section-6/sec6-statistics/sec6-statistics.component.ts
+++ src/app/components/section-6/sec6-statistics/sec6-statistics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Statistic } from 'src/app/models/statistics';
 
 @Component({
   selector: 'app-sec6-statistics',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sec6-statistics.component.css']
 })
 export class Sec6StatisticsComponent implements OnInit {
+
+  statistics: Statistic[] = [
+    { title: 'Total supply', value: '284,570,158' },
+    { title: 'Burned to date', value: '426,692,888' },
+    { title: 'Market cap', value: '$2.3 billion' },
+    { title: 'Current emissions', value: '14.25/block' }
+  ]
 
   constructor() { }
 
