6896d030662e230482c451fa74038198f1ed6976
Matteo Musti <94116882+MatteoMusti@users.noreply.github.com>
2022-04-20 19:38:00 +0200

    add: wallet component

--- src/app/child-route-module/child-routing.module.ts
+++ src/app/child-route-module/child-routing.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { FarmsContainerComponent } from '../components/farms/farms-container/farms-container.component';
 import { SectionTradeNowComponent } from '../components/section-trade-now/section-trade-now.component';
+import { WalletComponent } from './wallet/wallet.component';
 
 const routes: Routes = [
   /*scrivere rotte child*/
   { path: 'farms', component: FarmsContainerComponent},
-  { path: 'trade', component: SectionTradeNowComponent}
-
+  { path: 'trade', component: SectionTradeNowComponent},
+  { path: 'wallet', component: WalletComponent}
 ]
 
 @NgModule({
