06a8e8f1f50fa891b6e2aa6b4816d36fd66f1911
Alessia Russo <r.alessia2494@gmail.com>
2022-04-20 12:00:33 +0200

    fixroutingchild  and add buttonhome

--- src/app/child-route-module/child-routing.module.ts
+++ src/app/child-route-module/child-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { FarmsContainerComponent } from '../components/farms/farms-container/farms-container.component';
+import { SectionTradeNowComponent } from '../components/section-trade-now/section-trade-now.component';
 
-/*scrivere rotte child*/
+const routes: Routes = [
+  /*scrivere rotte child*/
+  { path: 'farms', component: FarmsContainerComponent},
+  { path: 'trade', component: SectionTradeNowComponent}
+
+]
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule,/*ruotermodule.forchild*/],
+  imports: [CommonModule, RouterModule.forChild(routes)],
   exports: [RouterModule,]
 })
 export class ChildRoutingModule { }
