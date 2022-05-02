b688e0b6919f2bc15d27ae8dc1e3e3320bce8998
Benny Capozzo <bennycapozzo@outlook.com>
2022-05-02 17:21:47 +0200

    fix wallet da trade

--- src/app/components/section-trade-now/section-trade-now.component.ts
+++ src/app/components/section-trade-now/section-trade-now.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { modalService } from 'src/app/services/modal/modal.service';
 
 @Component({
   selector: 'app-section-trade-now',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SectionTradeNowComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modal: modalService) { }
 
   ngOnInit(): void {
+  }
+
+  modalOn(){
+    this.modal.setModal(true);
   }
 
 }
