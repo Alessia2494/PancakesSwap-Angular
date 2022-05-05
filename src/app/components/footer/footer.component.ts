578d0e3b3d7a984e415f5b909a5385299f0262f9
Daniele <d.delluomo99@gmail.com>
2022-05-05 16:42:48 +0200

    aggiunto language al footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { language } from 'src/app/models/modelNavbar';
+
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +8,94 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+
+  languages:language[]=[
+    {
+      linguage:"العربي"
+    },
+    {
+      linguage: "বাংলা"
+    },
+    {
+      linguage: "English"
+    },
+    {
+      linguage: "Deutsch"
+    },
+    {
+      linguage: "Ελληνικά"
+    },
+    {
+      linguage:"Español"
+    },
+    {
+      linguage:"Suomalainen"
+    },
+    {
+      linguage:"Filipino"
+    },
+    {
+      linguage:"Français"
+    },
+    {
+      linguage:"हिंदी"
+    },
+    {
+      linguage:"Magyar"
+    },
+    {
+      linguage:"Bahasa Indonesia"
+    },
+    {
+      linguage:"Italiano"
+    },
+    {
+      linguage:"日本語"
+    },
+    {
+      linguage:"한국어"
+    },
+    {
+      linguage:"Nederlands"
+    },
+    {
+      linguage:"Polski"
+    },
+    {
+      linguage:"Português(Brazil)"
+    },
+    {
+      linguage:"Português"
+    },
+    {
+      linguage:"Română"
+    },
+    {
+      linguage:"Русский"
+    },
+    {
+      linguage:"Svenska"
+    },
+    {
+      linguage:"தமிழ்"
+    },
+    {
+      linguage:"Türkçe"
+    },
+    {
+      linguage:"Українська"
+    },
+   {
+    linguage:"Tiếng Việt"
+   },
+   {
+    linguage:"简体中文"
+   },
+   {
+    linguage:"繁體中文"
+   }
+ ]
+
 
   constructor() { }
 
